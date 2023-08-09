d3844c7969e390278ae44f3493f2ca5f76bb86a5
Monika171 <monikarabha171@gmail.com>
2023-08-09 14:38:16 +0900

    test for add new book

--- src/tests/Feature/BookControllerTest.php
+++ src/tests/Feature/BookControllerTest.php
@@ -12,6 +12,8 @@ class BookControllerTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
+
+    // List Books
     public function test_books_can_be_viewed(){
         $response = $this->get('/');
         $response->assertStatus(200);
@@ -25,5 +27,20 @@ class BookControllerTest extends TestCase
         foreach ($books as $book) {
             $response->assertSee($book->title);
         }
+    }
+
+    // Add a book to the list
+    public function test_new_book_can_be_added() {
+        // Disabling CSRF protection for this test
+        $this->withoutMiddleware();
+
+        $bookData = [
+            'title' => 'Title 2023',
+            'author' => 'Author new',
+        ];
+        $response = $this->post('/books', $bookData);
+        $response->assertStatus(302);
+        $this->assertCount(1, Book::all());
+        $this->assertDatabaseHas('books', $bookData);
     }
 }
